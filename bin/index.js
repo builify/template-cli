4d6cf6ea890241ff9d93e8ee01e5551473585773
TheDeveloperXYZ <contact@thedeveloper.xyz>
2016-11-01 14:43:49 +0200

    Small update

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 #! /usr/bin/env node
 const path = require('path');
+const _isFunction = require('lodash/isfunction');
 const globals = require('./globals');
 const generatedData = require('./generated-data');
 const parseOptions = require('./parse-options');
@@ -10,23 +11,26 @@ const ParseHTML = require('./parse-html');
 const currentDir = globals.currentDir;
 
 // Get CLI options.
-const options = globals.cli.parse();
+// Sometimes parse is not function. Seems to be error with cmd package.
+if (_isFunction(globals.cli.parse)) {
+  const options = globals.cli.parse();
 
-if (parseOptions(options)) {
-  const optionsSource = path.normalize(options.src);
-  const optionsStylesheet = path.normalize(options.stylesheet);
-  const optionsOutput = options.output;
-  const optionsPictures = options.pictures || false;
+  if (parseOptions(options)) {
+    const optionsSource = path.normalize(options.src);
+    const optionsStylesheet = path.normalize(options.stylesheet);
+    const optionsOutput = options.output;
+    const optionsPictures = options.pictures || false;
 
-  // Template paths.
-  const buildDir = path.join(currentDir, path.normalize(optionsOutput));
+    // Template paths.
+    const buildDir = path.join(currentDir, path.normalize(optionsOutput));
 
-  // Where magic happens.
-  const stylesheet = new GetStyling(optionsStylesheet);
-  const html = new ParseHTML(
-    path.join(currentDir, optionsSource),
-    stylesheet.getStylings(generatedData(globals.manifest)),
-    buildDir,
-    optionsPictures
-  );
+    // Where magic happens.
+    const stylesheet = new GetStyling(optionsStylesheet);
+    const html = new ParseHTML(
+      path.join(currentDir, optionsSource),
+      stylesheet.getStylings(generatedData(globals.manifest)),
+      buildDir,
+      optionsPictures
+    );
+  }
 }
