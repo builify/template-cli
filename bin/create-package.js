57eec77c4f25c8475cdb6e13cdfe41e50dc7ddd8
Genert <orggenert@gmail.com>
2017-01-19 00:27:47 +0200

    Wrapper for local file

--- bin/create-package.js
+++ bin/create-package.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const zip = new require('node-zip')();
 
-const packageFileName = 'arkio.builify';
+const packageFileName = 'arkio.builify.js';
 
 function createPackage (manifestObject, buildDir, fileName = 'manifest.json') {
   zip.file(fileName, JSON.stringify(manifestObject, null, 2));
@@ -12,8 +12,9 @@ function createPackage (manifestObject, buildDir, fileName = 'manifest.json') {
     compression: 'DEFLATE'
   });
   const filePath = path.join(buildDir, packageFileName);
+  const fileText = `var __BUILIFY_TEMPLATE = "${data}"`;
 
-  fs.writeFileSync(filePath, data, 'binary');
+  fs.writeFileSync(filePath, fileText, 'binary');
 }
 
 module.exports = createPackage;
